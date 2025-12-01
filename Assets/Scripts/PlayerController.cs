db8374eb853515ed0e68fe68a3cf4a6fe3fdb513
Tim L <timlange2004@gmail.com>
2025-12-02 00:47:05 +0100

    NEW PLAYER IN LEGO CITY

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,22 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     [SerializeField] private float gravity = -9.81f;
-    [SerializeField] private float sensitivity = 10f;
-    
+
+    [SerializeField] private Transform cam;
+    [SerializeField] private float sensitivity = 5;
+
+
     private CharacterController controller;
     private Vector3 moveInput;
     private Vector3 lookInput;
     private Vector3 velocity;
+
+    private float pitch;
     
     void Start()
     {
-        controller = GetComponent<CharacterController>();    
+        controller = GetComponent<CharacterController>();   
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -23,14 +29,24 @@ public class PlayerController : MonoBehaviour
         Debug.Log($"moveInput: {moveInput}");
     }
 
-    public void OnRotate(InputAction.CallbackContext context)
+    public void onLook(InputAction.CallbackContext context)
     {
         lookInput = context.ReadValue<Vector2>();
-        Debug.Log($"lookInput: {lookInput}");
+        Debug.Log($"Are you looking Son? {lookInput}");
     }
+    
     void Update()
     {
         Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        move = transform.TransformDirection(move);
         controller.Move(move * speed * Time.deltaTime);
+        
+        Vector2 look = lookInput * sensitivity * Time.deltaTime;
+        
+        transform.Rotate(Vector3.up * look.x);
+        
+        pitch -= look.y;
+        pitch = Mathf.Clamp(pitch, -90, 90);
+        cam.localRotation = Quaternion.Euler(pitch, 0f, 0f);
     }
 }

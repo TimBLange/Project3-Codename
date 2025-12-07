9e67b3a6ba4243fc326f9bcb24b4ce201d33e626
Lukas Hilgers <hilgers2905@gmail.com>
2025-12-07 18:02:10 +0100

    Update Menu.cs

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,7 +6,7 @@ public class Menu : MonoBehaviour
 {
     public void PlayGame() 
     {
-        SceneManager.LoadSceneAsync("House");
+        SceneManager.LoadSceneAsync(1);
     }
     
     void Start()

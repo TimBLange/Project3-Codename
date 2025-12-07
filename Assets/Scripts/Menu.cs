ca42605030fc644432c5dd553dd963f74c743b32
Lukas Hilgers <hilgers2905@gmail.com>
2025-12-07 17:59:05 +0100

    script menu
    scene loading for scene 1

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
-
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void PlayGame() 
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         

e860ba710f2a773e96a959069c2ae7fb67e5c553
Lukas Hilgers <hilgers2905@gmail.com>
2026-01-15 19:10:29 +0100

    footsteps

---
+++ Assets/Scripts/footsteps.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class footsteps : MonoBehaviour
+{
+    public AudioSource footstepsSound;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            footstepsSound.enabled = true;
+        }
+        else
+        {
+            footstepsSound.enabled = false;
+        }
+    }
+}

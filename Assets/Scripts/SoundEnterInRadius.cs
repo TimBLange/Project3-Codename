9615ceb1836d09d2e14c98cd265ea865bddefa3d
Tim L <timlange2004@gmail.com>
2026-01-20 18:09:41 +0100

    sound in school fixed

--- Assets/Scripts/SoundEnterInRadius.cs
+++ Assets/Scripts/SoundEnterInRadius.cs
@@ -1,19 +1,37 @@
 using System;
 using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
 
 
 public class SoundEnterInRadius : MonoBehaviour
 {
     public AudioSource Sound;
+    public AudioSource SoundDampened;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "SoundVol")
+        {
         Sound.Play();
+        SoundDampened.Stop();
+        }
+        else
+        {
+            return;
+        }
+        
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Sound.Stop();
+        if (other.tag == "SoundVol")
+        {
+            Sound.Stop();
+            SoundDampened.Play();
+        }
+        else
+        {
+            return;
+        }
     }
 }
